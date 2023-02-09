395422dd04759c8fbe2c37ee27290deaab15f9f7
blakeshelley10 <80291937+blakeshelley10@users.noreply.github.com>
2023-02-08 20:11:30 -0500

    Front end
    solves issue for login/registration

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string= "fa fa-eye";
+
+  hideShowPass(){
+
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye-slash" : this.eyeIcon = "fa-eye";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+}
